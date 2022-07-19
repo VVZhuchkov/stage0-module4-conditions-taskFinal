7a6f5b5cedc8bf0f848d0ab20cc6b3ddeb43e29d
VVZhuchkov <vzhuchkov@tut.by>
2022-07-19 13:31:07 +0300

    Without last task.

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,13 +1,12 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public static void main(String[] args) {
-        swap(1, 4);
-    }
 
-    public static void swap(int first, int second) {
-        first = first ^ second;
+    public void swap(int first, int second) {
+    first = first ^ second;
+    second = first ^ second;
+    first = first ^second;
         System.out.println(first);
-
+        System.out.println(second);
     }
 }
